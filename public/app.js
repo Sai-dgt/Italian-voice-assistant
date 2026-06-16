04e625eeaee4fa857d981037268fc01caa8b6a8b
Alexandre <alxtsz@gmail.com>
2026-06-16 18:05:19 +0200

    app.js update

--- public/app.js
+++ public/app.js
@@ -1,8 +1,89 @@
 const bars = document.querySelectorAll('.wave-bar');
 bars.forEach((bar,index)=>{
-
 bar.style.animationDelay = `${index*0.05}s`;
-
-
-
 });
+let isRecording= false
+const mike_button=document.querySelector('.microphone');
+let media_recorder ;
+let chunks= [];
+let stream
+const LevelButton = document.querySelectorAll('.level-button')
+const levels=["beginner","intermediate","advanced"]
+let currentLevel="intermediate"
+
+LevelButton.forEach((button,index)=>{
+button.addEventListener("click",function(){
+currentLevel=levels[index]
+})
+})
+    
+mike_button.addEventListener("click",async function () {
+
+   try{
+   
+     if (isRecording===false){isRecording=true  ;
+    stream= await navigator.mediaDevices.getUserMedia({audio:true});
+    chunks=[]
+    media_recorder=new MediaRecorder (stream)
+   
+    media_recorder.ondataavailable=function(event){
+     chunks.push(event.data)
+    };
+    
+    media_recorder.onstop=function(){
+    isRecording=false
+    stream.getTracks().forEach(track => track.stop())        
+   
+    const blob = new Blob (chunks, {type:'audio/webm'})
+    const formData = new FormData()
+    formData.append('audio',blob,'recording.webm')
+    fetch ('/transcribe',{
+     method:'POST',
+     body: formData
+    })
+    .then(response=> response.text())
+    .then(transcript=> fetch ('/chat',{
+     method:"POST",
+     headers:{"Content-Type":"application/json"},
+     body:JSON.stringify({
+     message:transcript,
+     level:currentLevel,
+     history:[]
+     })
+    }))
+    .then(response=> response.text())
+    .then(reply=> fetch('/speak',{
+     method:'POST',
+     headers:{"Content-Type":"application/json"},
+     body:JSON.stringify({text:reply})
+
+    }))
+    .then(response =>response.blob())
+    .then(blob=>{
+
+        const audio= new Audio(URL.createObjectURL(blob))
+        audio.play()
+        audio.onplaying=function (){
+           bars.forEach(bar=>bar.style.animationPlayState="running")
+           audio.onended=function(){
+           bars.forEach(bar=>bar.style.animationPlayState="paused")
+
+           }
+
+        }
+    })
+    };
+    media_recorder.start()
+    setTimeout(()=>media_recorder.stop(),8000);
+    
+   
+     }else {
+        media_recorder.stop()
+     }
+   }catch (error){
+    console.log("Mic denied");
+
+    
+   }
+})
+
